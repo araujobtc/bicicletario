f943d919a5eaff8cf770c4ae2f77e35b57fc7c13
araujobtc <isabelletecn@gmail.com>
2025-11-05 23:55:19 -0300

    Repositorys

---
+++ src/main/java/com/bikeunirio/bicicletario/aluguel/repository/CiclistaRepository.java
@@ -0,0 +1,11 @@
+package com.bikeunirio.bicicletario.aluguel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bikeunirio.bicicletario.aluguel.entity.Ciclista;
+
+@Repository
+public interface CiclistaRepository extends JpaRepository<Ciclista, Long> {
+
+}
---
+++ src/main/java/com/bikeunirio/bicicletario/aluguel/repository/FuncionarioRepository.java
@@ -0,0 +1,11 @@
+package com.bikeunirio.bicicletario.aluguel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bikeunirio.bicicletario.aluguel.entity.Funcionario;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+}
