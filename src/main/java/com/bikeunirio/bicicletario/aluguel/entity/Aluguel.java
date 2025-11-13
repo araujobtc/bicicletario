6a0cd58b3bf5881f07bc062d1ab8027ef13e01bc
araujobtc <isabelletecn@gmail.com>
2025-11-13 05:34:00 -0300

    update

--- src/main/java/com/bikeunirio/bicicletario/aluguel/entity/Aluguel.java
+++ src/main/java/com/bikeunirio/bicicletario/aluguel/entity/Aluguel.java
@@ -20,7 +20,9 @@ import jakarta.persistence.Table;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "alugueis")
 public class Aluguel {
@@ -62,63 +64,4 @@ public class Aluguel {
     @Column(nullable = false)
     private StatusAluguel status;
 
-    public Long getId() {
-        return id;
-    }
-
-    public Ciclista getCiclista() {
-        return ciclista;
-    }
-
-    public void setCiclista(Ciclista ciclista) {
-        this.ciclista = ciclista;
-    }
-
-    public Long getBicicletaId() {
-        return bicicletaId;
-    }
-
-    public void setBicicletaId(Long bicicletaId) {
-        this.bicicletaId = bicicletaId;
-    }
-
-    public Cartao getCartao() {
-        return cartao;
-    }
-
-    public void setCartao(Cartao cartao) {
-        this.cartao = cartao;
-    }
-
-    public LocalDateTime getDataInicio() {
-        return dataInicio;
-    }
-
-    public void setDataInicio(LocalDateTime dataInicio) {
-        this.dataInicio = dataInicio;
-    }
-
-    public LocalDateTime getDataFim() {
-        return dataFim;
-    }
-
-    public void setDataFim(LocalDateTime dataFim) {
-        this.dataFim = dataFim;
-    }
-
-    public Double getValor() {
-        return valor;
-    }
-
-    public void setValor(Double valor) {
-        this.valor = valor;
-    }
-
-    public StatusAluguel getStatus() {
-        return status;
-    }
-
-    public void setStatus(StatusAluguel status) {
-        this.status = status;
-    }
 }
